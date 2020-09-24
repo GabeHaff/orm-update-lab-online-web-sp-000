72eec4adb76ae38a1691021d54d20c8385a21238
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-09-24 20:55:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,19 @@ def self.drop_table
   DB[:conn].execute(sql)
 end 
 
+def save 
+  if self.id 
+    self.update 
+  else 
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES(?,?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execite("SELECT last_insert_rowid() FROM students")[0][0] 
 
+end 
 
 
 
