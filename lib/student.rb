971717ec599111b05641384fb95e4679aecd2b9b
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-09-24 21:18:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,8 +54,12 @@ def self.create(name, grade)
   student 
 end 
 
-def self.new_from_db
-  
+def self.new_from_db(row)
+  new_student = self.new 
+  new_student.id = row[0]
+  new_student.name = row[1]
+  new_student.grade = row[2]
+  new_student
 end 
 
   # Remember, you can access your database connection anywhere in this class
